ecb6fd34c87ad6c141f8ec9fe3fee109caec7bdd
PhantomPink <PhantomPink@users.noreply.github.com>
2020-10-23 00:07:58 +0200

    Updates

---
+++ precache-manifest.d7d978058add4b9720fcdb0524a9c134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22561938ff5257a25bbaf9107034ad43",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "5f188541671bfca807d3",
+    "url": "/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c37798a2004f58104d68",
+    "url": "/face-recognition-app/static/css/main.7f1b072b.chunk.css"
+  },
+  {
+    "revision": "5f188541671bfca807d3",
+    "url": "/face-recognition-app/static/js/2.e8a19778.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-app/static/js/2.e8a19778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37798a2004f58104d68",
+    "url": "/face-recognition-app/static/js/main.b773b2f3.chunk.js"
+  },
+  {
+    "revision": "c22eb5eba8663bd91f02",
+    "url": "/face-recognition-app/static/js/runtime-main.a073d0dd.js"
+  }
+]);
